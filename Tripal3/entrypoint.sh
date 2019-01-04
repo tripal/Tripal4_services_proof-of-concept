00f10b4333a6a8426d89b71212f53971cd4f31ed
Ethy Cannon <ekcannon@iastate.edu>
2019-01-04 07:53:39 -0600

    Initial import

---
+++ Tripal3/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+echo ${DB_HOST};
+set -o xtrace
+chmod 777 /var/www/html/sites/default/settings.php
+ls -l /var/www/html/sites/default
+sed -i "s/'database' => 'drupal'/'database' => '$DB_NAME'/g" /var/www/html/sites/default/settings.php
+sed -i "s/'username' => 'postgres'/'username' => '$PGUSER'/g" /var/www/html/sites/default/settings.php
+sed -i "s/'password' => ''/'password' => '$POSTGRES_PASSWORD'/g" /var/www/html/sites/default/settings.php
+sed -i "s/'host' => 'localhost'/'host' => '$DB_HOST'/g" /var/www/html/sites/default/settings.php
+sed -i "s/'port' => '' => 'port'/'$DB_PORT'/g" /var/www/html/sites/default/settings.php
+chmod 444 /var/www/html/sites/default/settings.php
+
+php-fpm -D
+nginx -g 'daemon off;'
