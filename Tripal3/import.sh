1b187ba318d1ad5950f01878ed14eba74b2062ae
Ethy Cannon <ekcannon@iastate.edu>
2019-01-04 11:39:23 -0600

    Moved to db-init-scripts/

--- Tripal3/import.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-if [ "$( pqsl -U postgres postgres -tAc "SELECT 1 FROM pg_database WHERE datname='drupal'" )" != '1' ]
-then
-    echo "Database drupal does not exist, creating it"
-    createdb -U postgres drupal
-    psql -U postgres drupal < /pgexp/drupalexport.pgsql
-else
-    echo "Database drupal has already been configured"
-fi
