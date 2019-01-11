be0af035f325337b322a50ab3cc4c2abbd9a15c9
jwest60 <jwest2178@gmail.com>
2019-01-11 09:12:55 -0800

    service now calls database functions

--- Services/entity/service.py
+++ Services/entity/service.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template,  request, json
+from database import *
 
 app = Flask(__name__)
 
@@ -11,27 +12,28 @@ def hello():
 # Tell Drupal about it
 
 # Designed full route: /entity/{version}
-@app.route('/entity/<version>/')
+@app.route('/entity/<version>/', methods=['GET'])
 def get_content_types(version):
-    return version
+  if request.method == 'GET':
+    return list_entity_types()
 
 # Designed full route: /entity/{version}/{entity_type}
 @app.route('/entity/<version>/<int:entity_type>/', methods=['GET'])
 def get_entities_by_type(version, entity_type):
   if request.method == 'GET':
-    return str(entity_type)
+    return list_entities_by_type(entity_type)
 
 # Designed full route: /entity/{version}/{entity_type}/{entity_id}
 @app.route('/entity/<version>/<int:entity_type>/<int:entity_id>/', methods=['GET'])
 def get_entity(version, entity_type, entity_id):
   if request.method == 'GET':
-    return str(entity_id)
+    return list_entity(entity_type, entity_id)
 
 # Designed full route: /entity/{version}/{entity_type}/{entity_id}/{field_name}
 @app.route('/entity/<version>/<int:entity_type>/<int:entity_id>/<field_name>/', methods=['GET'])
 def get_entity_field_data(version, entity_type, entity_id, field_name):
   if request.method == 'GET':
-    return field_name
+    return list_entity_field_data(entity_type, entity_id, field_name)
 
 @app.route('/import/entities/<provider>/<int:entity_type>', methods=['POST'])
 def post_entity(provider, entity_type):
