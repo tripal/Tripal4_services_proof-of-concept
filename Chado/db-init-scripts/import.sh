9d586f1e43dfb786726b683cb424d9ec811efcd8
Stephen Ficklin <spficklin@gmail.com>
2019-01-11 09:27:26 -0800

    Reworkgin D8 image

--- Chado/db-init-scripts/import.sh
+++ Chado/db-init-scripts/import.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/env sh
-if [ "$( psql -U postgres postgres -tAc "SELECT 1 FROM pg_database WHERE datname='chado'" )" != '1' ]
-then
-    echo "Database chado does not exist, creating it"
-    # su - postgres
-    # createuser -P chado_user
-    # createdb chado -O chado_user
-    # ALTER USER chado_user WITH PASSWORD 'password';
-    psql -U chado_user < /docker-entrypoint-initdb.d/sql/1_all_chado_tables.sql
-    psql -U chado_user < /docker-entrypoint-initdb.d/sql/2_load_basic_tables.sql
-    psql -U chado_user < /docker-entrypoint-initdb.d/sql/3_load_germplasm_1.sql
-    psql -U chado_user < /docker-entrypoint-initdb.d/sql/4_load_germplasm_2.sql
-    psql -U chado_user < /docker-entrypoint-initdb.d/sql/5_load_gene_model.sql
-else
-    echo "Database chado has already been configured"
-fi
+psql -U chado_user < /docker-entrypoint-initdb.d/sql/1_all_chado_tables.sql
+psql -U chado_user < /docker-entrypoint-initdb.d/sql/2_load_basic_tables.sql
+psql -U chado_user < /docker-entrypoint-initdb.d/sql/3_load_germplasm_1.sql
+psql -U chado_user < /docker-entrypoint-initdb.d/sql/4_load_germplasm_2.sql
+psql -U chado_user < /docker-entrypoint-initdb.d/sql/5_load_gene_model.sql
--- Drupal8/entrypoint.sh
+++ Drupal8/entrypoint.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env sh
-echo "entrypoint.sh";
-echo ${DB_HOST};
-set -o xtrace
-chmod 777 /var/www/html/web/sites/default/settings.php
-ls -l /var/www/html/web/sites/default
-sed -i "s/'database' => 'drupal'/'database' => '$DB_NAME'/g" /var/www/html/web/sites/default/settings.php
-sed -i "s/'username' => 'postgres'/'username' => '$PGUSER'/g" /var/www/html/web/sites/default/settings.php
-sed -i "s/'password' => ''/'password' => '$POSTGRES_PASSWORD'/g" /var/www/html/web/sites/default/settings.php
-sed -i "s/'host' => 'localhost'/'host' => '$DB_HOST'/g" /var/www/html/web/sites/default/settings.php
-sed -i "s/'port' => '' => 'port'/'$DB_PORT'/g" /var/www/html/web/sites/default/settings.php
-chmod 444 /var/www/html/web/sites/default/settings.php
+# Drupal Install
 
-php-fpm -D
-nginx -g 'daemon off;'
+#drush si -y --db-url=pgsql://drupal8_user@drupal8_db/drupal --account-name=admin --account-pass=admin --site-mail=admin@gmail.com --site-name='Drupal D8'
+
+# echo "entrypoint.sh";
+# echo ${DB_HOST};
+# set -o xtrace
+# chmod 777 /var/www/html/web/sites/default/settings.php
+# ls -l /var/www/html/web/sites/default
+# sed -i "s/'database' => 'drupal'/'database' => '$DB_NAME'/g" /var/www/html/web/sites/default/settings.php
+# sed -i "s/'username' => 'drupal8_user'/'username' => '$PGUSER'/g" /var/www/html/web/sites/default/settings.php
+# sed -i "s/'password' => 'example'/'password' => '$POSTGRES_PASSWORD'/g" /var/www/html/web/sites/default/settings.php
+# sed -i "s/'host' => 'drupal8_db'/'host' => '$DB_HOST'/g" /var/www/html/web/sites/default/settings.php
+# sed -i "s/'port' => '5432' => 'port'/'$DB_PORT'/g" /var/www/html/web/sites/default/settings.php
+# chmod 444 /var/www/html/web/sites/default/settings.php
+
+# php-fpm -D
+# nginx -g 'daemon off;'
