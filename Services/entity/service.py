50f8bb473a6326c94b617d87aa8a3382f8aa8490
Katheryn Buble <ksbuble@gmail.com>
2019-01-11 07:48:12 -0800

    Added POST and GET checks

--- Services/entity/service.py
+++ Services/entity/service.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,  request, json
 
 app = Flask(__name__)
 
@@ -16,26 +16,31 @@ def get_content_types(version):
     return version
 
 # Designed full route: /entity/{version}/{entity_type}
-@app.route('/entity/<version>/<int:entity_type>/')
+@app.route('/entity/<version>/<int:entity_type>/', methods=['GET'])
 def get_entities_by_type(version, entity_type):
+  if request.method == 'GET':
     return str(entity_type)
 
 # Designed full route: /entity/{version}/{entity_type}/{entity_id}
-@app.route('/entity/<version>/<int:entity_type>/<int:entity_id>/')
+@app.route('/entity/<version>/<int:entity_type>/<int:entity_id>/', methods=['GET'])
 def get_entity(version, entity_type, entity_id):
+  if request.method == 'GET':
     return str(entity_id)
 
 # Designed full route: /entity/{version}/{entity_type}/{entity_id}/{field_name}
-@app.route('/entity/<version>/<int:entity_type>/<int:entity_id>/<field_name>/')
+@app.route('/entity/<version>/<int:entity_type>/<int:entity_id>/<field_name>/', methods=['GET'])
 def get_entity_field_data(version, entity_type, entity_id, field_name):
+  if request.method == 'GET':
     return field_name
 
 @app.route('/import/entities/<provider>/<int:entity_type>', methods=['POST'])
 def post_entity(provider, entity_type):
+  if request.method == 'POST':
     return "placeholder\n"
 
 @app.route('/import/fields/<int:entity_type>', methods=['POST'])
 def post_entity_fields(entity_type):
+  if request.method == 'POST':
     return "placeholder\n"
 
 
